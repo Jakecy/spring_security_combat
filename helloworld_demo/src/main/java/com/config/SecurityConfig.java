5bfbb0c529b1047bac3c2dcadc96e7fdd8d4b130
chihaojie <woltwhite@163.com>
2018-06-24 15:54:31 +0800

    使用main方法演示认证过程

--- helloworld_demo/src/main/java/com/config/SecurityConfig.java
+++ helloworld_demo/src/main/java/com/config/SecurityConfig.java
@@ -1,21 +1,43 @@
 package com.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.security.MyLogoutHandler;
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig<MyAuthenticationProvider> extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private MyLogoutHandler  MyLogoutHandler;
+	@Autowired
+	private MyAuthenticationProvider  MyAuthenticationProvider;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
 				.antMatchers("/css/**", "/index").permitAll()		
-				.antMatchers("/user/**").hasRole("USER")			
+				.antMatchers("/user/**").hasRole("USER")
 				.and()
 			.formLogin()
-				.loginPage("/login").failureUrl("/login-error");	
+				  .loginPage("/static/login.html")
+				  .permitAll()
+		          .and()
+		    .logout()
+					.logoutUrl("/my/logout")                                                
+					.logoutSuccessUrl("/my/index")                                          
+					//.logoutSuccessHandler(MyLogoutHandler)                              
+					.invalidateHttpSession(true)                                             
+					.addLogoutHandler(MyLogoutHandler)                                         
+					.deleteCookies("JSESSIONID")                                       
+					.and()
+		    .sessionManagement().invalidSessionUrl("/invalidSession.html");//当sessionID无效时的跳转路径
 	}
 
 	@Autowired
@@ -23,6 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth
 			.inMemoryAuthentication()
 				.withUser("user").password("password").roles("USER");
+		auth.authenticationProvider((AuthenticationProvider) MyAuthenticationProvider);
 			    
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
